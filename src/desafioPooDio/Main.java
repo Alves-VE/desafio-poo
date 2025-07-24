2578cf31ff377bad72323c2fa887720a7e8949de
Alves-VE <victor86ribeiro@gmail.com>
2025-07-24 14:52:34 -0300

    teste

--- src/desafioPooDio/Main.java
+++ src/desafioPooDio/Main.java
@@ -16,7 +16,7 @@ public class Main {
 		curso2.setTitulo("curso Js");
 		curso2.setDescricao("descrição curso Js");
 		curso2.setCargaHoraria(4);
-		//teste 1d2
+		//teste 1
 		Mentoria mentoria = new Mentoria();
 		mentoria.setTitulo("Mentoria Java");
 		mentoria.setDescricao("Descrição mentoria Java");
